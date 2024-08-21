0ae503e6564582731ec5b6d448a19435a4650cbc
Hermez Jaramillo <angelo_stk06@hotmail.com>
2024-08-21 17:50:58 -0500

    feat: adiciono estrutura del modelo-aplicacion

--- notas/models.py
+++ notas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+#creacion de la estructura BD de la aplicacion
+class Nota(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=200)
+    creado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.titulo}, {self.descripcion}'
