7856574c8d03e9d989ebe91367dc6c6739eb89ca
Hermez Jaramillo <angelo_stk06@hotmail.com>
2024-08-28 20:56:10 -0500

    feat: agrego funciones crear y listar en views

--- notas/views.py
+++ notas/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Nota
+from .forms import NotaForm
 
-def test(request):
-    return HttpResponse("Es un test app")
+
+def lista_notas(request):
+    notas = Nota.objects.all()  # .order_by('creado').reverse()
+    return render(request, 'listar_notas.html', {'notas': notas})
+
+
+def crear_nota(request):
+    if request.method == "POST":
+        form = NotaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_notas')
+    else:
+        form = NotaForm()
+    return render(request, 'crear_notas.html', {'form': form})
