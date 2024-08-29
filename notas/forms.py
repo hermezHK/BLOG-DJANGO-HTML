16db2de2caac2db0df3ada5e920dfc40250a2a7e
Hermez Jaramillo <angelo_stk06@hotmail.com>
2024-08-28 20:43:43 -0500

    feat: modifico campo descripcion models/agrego forms.py

---
+++ notas/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Nota
+
+
+class NotaForm(forms.ModelForm):
+    class Meta:
+        model = Nota
+        fields = ['titulo', 'contenido']
--- notas/models.py
+++ notas/models.py
@@ -7,8 +7,9 @@ from django.db import models
 
 class Nota(models.Model):
     titulo = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=200)
+    contenido = models.CharField(max_length=200)
     creado = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.titulo
