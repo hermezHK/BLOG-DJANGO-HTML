c36c0af67eb186219395170397707b39ee734c91
Hermez Jaramillo <angelo_stk06@hotmail.com>
2024-08-28 23:25:03 -0500

    feat: se adicionaron las rutas

--- notas/urls.py
+++ notas/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_notas, name='listar_notas'),
-    path('crear/', views.crear_nota, name='crear_notas'),
+    path('', views.listar_notas, name='listar_notas'),
+    path('crear/', views.crear_nota, name='crear_nota'),
+    path('actualizar/<int:pk>/', views.actualizar_nota, name='actualizar_nota'),
+    path('borrar/<int:pk>/', views.borrar_nota, name='borrar_nota'),
 ]
